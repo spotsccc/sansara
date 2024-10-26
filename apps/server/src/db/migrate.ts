2630753f5180c5622fe167687649f06a55627a4a
chernenkoik <chernenkoik@tutu.tech>
2024-10-26 20:43:56 -0300

    add ssl cert

--- apps/server/src/db/migrate.ts
+++ apps/server/src/db/migrate.ts
@@ -6,6 +6,10 @@ import * as schema from "./schema";
 
 const connection = new Client({
   connectionString: process.env["DATABASE_URL"],
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env["CA_CERT"],
+  },
 });
 
 console.log(process.env["DATABASE_URL"]);
