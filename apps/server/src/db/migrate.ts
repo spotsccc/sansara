70f46e5db8f51fc387e7c506d805fb5c92e44d82
chernenkoik <chernenkoik@tutu.tech>
2024-10-26 21:02:52 -0300

    debug ca_cert

--- apps/server/src/db/migrate.ts
+++ apps/server/src/db/migrate.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { Client } from "pg";
+import { Pool } from "pg";
 import * as schema from "./schema";
 
-const connection = new Client({
+const connection = new Pool({
   connectionString: process.env["DATABASE_URL"],
   ssl: {
     rejectUnauthorized: true,
