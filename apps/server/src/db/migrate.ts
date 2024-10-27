bedd3fde7557d7a4350cc84f5681d04ef24f77ee
chernenkoik <chernenkoik@tutu.tech>
2024-10-26 21:16:46 -0300

    debug ca_cert

--- apps/server/src/db/migrate.ts
+++ apps/server/src/db/migrate.ts
@@ -1,19 +1,21 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { Pool } from "pg";
+import { Client } from "pg";
 import * as schema from "./schema";
 
-const connection = new Pool({
-  connectionString: process.env["DATABASE_URL"],
+const connection = new Client({
+  host: process.env["DB_HOST"],
+  user: process.env["DB_USERNAME"],
+  password: process.env["DB_PASSWORD"],
+  port: Number(process.env["DB_PORT"]),
+  database: process.env["DB_NAME"],
+  connectionTimeoutMillis: 2000,
   ssl: {
     rejectUnauthorized: true,
     ca: process.env["DATABASE_CA"],
   },
 });
-
-console.log(process.env["DATABASE_URL"]);
-console.log(process.env["DATABASE_CA"]);
 
 connection
   .connect()
